a54f0b34cd0b3b9ab76e88fd936a11acbc745e84
RWorkshop-at-UF <rworkshop503@gmail.com>
2021-10-08 13:14:12 -0400

    Session 2

---
+++ session2/data/another_script.R
@@ -0,0 +1,10 @@
+
+common_func1<-function(){
+  "I use this function daily"
+}
+
+common_func2<-function(){
+  "I dislike flooding my main script"
+}
+
+common_data<-c("This is a string vector.", "I can", "call data", "in other script","as well")
